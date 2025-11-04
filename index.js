db5fe9063b23f4bc480aa75cdcf7cffcace8fa3d
Umar <umarshyk683@gmail.com>
2025-11-04 12:58:40 +0500

    update credit

--- index.js
+++ index.js
@@ -82,6 +82,8 @@ const categoryRoutes = require("./routes/bg_catg");
 const creditRoutes = require("./routes/credits");
 const router = require("./routes/pdf_mail");
 const paypalRoutes = require("./routes/paypal");
+const userRoutes = require("./routes/user.routes");
+app.use("/api", userRoutes);
 
 // CORS package
 const cors = require("cors");
